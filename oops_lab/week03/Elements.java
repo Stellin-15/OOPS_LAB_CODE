4d105ffa14b695d4e816025681a19eef4ef53395
Stellin-15 <123811154+Stellin-15@users.noreply.github.com>
2023-09-26 08:22:14 +0400

    week3 codes added

---
+++ oops_lab/week03/Elements.java
@@ -0,0 +1,84 @@
+import java.util.*;
+public class Elements {
+    int atomic_number;
+    String name_element;
+    String Symbol_element;
+    String Clas;
+    double atomic_weight;
+
+        Elements(int AN,String name,String sym,String clas,double AW)//constrctor 
+        {
+                atomic_number=AN;
+                name_element=name;
+                Symbol_element=sym;
+                Clas=clas;
+                atomic_weight=AW;
+        }
+
+public void display()
+{
+    System.out.println("Atomic number: "+atomic_number);
+    System.out.println("Element name: "+name_element);
+    System.out.println("Chemical Symbol: "+Symbol_element);
+    System.out.println("Class: "+Clas);
+    System.out.println("Atomic: "+atomic_weight);
+}
+
+public void AW_above15()
+{
+        if(atomic_number>15)
+        {
+                display();
+        }
+}
+
+
+        public static void main(String args[]){
+            Scanner sc = new Scanner(System.in);
+            int num;
+            System.out.println("Enter the number of elements: ");
+            num=sc.nextInt();
+
+            Elements pt []= new Elements[num];
+
+            for(int i=0;i<num;i++)
+            {
+                    System.out.println("Enter the atomic number of the element: ");
+                    int AN = sc.nextInt();
+
+                    sc.nextLine();
+                    System.out.println("Enter the name of the element: ");
+                    String name = sc.nextLine();
+                    System.out.println("Enter the chemical symbol of the element: ");
+                    String sym = sc.nextLine();
+                    System.out.println("Enter the class of the element: ");
+                    String clas = sc.nextLine();
+                    System.out.println("Enter the atomic weight of the element: ");
+                    double AW = sc.nextDouble();
+
+                    pt[i] = new Elements(AN,name,sym,clas,AW);
+
+                
+
+
+            }
+
+            for(int i =0;i<num;i++)
+            {
+                  pt[i].display();
+                System.out.println(" ");
+            }
+
+System.out.println("The elements above the atomic number 15: ");
+            for(int i =0;i<num;i++)
+            {
+                  pt[i].AW_above15();
+                System.out.println(" ");
+            }
+
+
+
+
+        }
+    
+}
