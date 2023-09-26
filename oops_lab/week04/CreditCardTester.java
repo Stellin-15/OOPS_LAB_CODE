679b3edfa8aad7aa7920e8e54c85f7952c5a9a6f
Stellin-15 <123811154+Stellin-15@users.noreply.github.com>
2023-09-26 08:21:36 +0400

    question 1

---
+++ oops_lab/week04/CreditCardTester.java
@@ -0,0 +1,209 @@
+import java.util.*;
+
+class Creditcard{
+private String name;
+private String cardNo;
+private boolean enabled;
+private int pin;
+private String expiryMonth;
+private int cardType;
+double creditLimit;
+// constructor 
+    Creditcard(String nam, String CN,boolean enabled,int PIN,String EM,int CT,double CL )
+        {
+            name = nam;
+            cardNo = CN;
+            this.enabled=enabled;
+            pin=PIN;
+            expiryMonth=EM;
+            cardType=CT;
+            creditLimit=CL;
+        }
+    //getter 
+    public int getpin(){
+        return pin ;
+    }
+
+    //pin check
+    public void check(int p){
+        if(getpin()==p) {options();}
+        else {System.out.println("Entered PIN is wrong");};
+    }
+    //options 
+    public void options(){
+        Scanner sc = new Scanner(System.in);
+        int index =1;
+        while(index !=0){
+            System.out.println("Choose an option: ");
+            System.out.println("Display - 1 \nChange pin - 2 \ntransaction - 3\nchange card status - 4\nExit - 5 ");
+            int opt = sc.nextInt();
+
+            switch(opt){
+                case 1:
+                    Display();
+                    break;
+                case 2:
+                    changepin();
+                    break;
+                case 3:
+                    transaction();
+                    break;
+                case 4:
+                    changestatus();
+                    break;
+                case 5:
+                index =0;
+                    break;
+                default:
+                    System.out.println("error!!");
+            }
+        }
+    
+    }
+    // display function 
+    public void Display(){
+
+        System.out.println("NAME : "+name);
+        System.out.println("CARD NUMBER : "+cardNo);
+        System.out.println("CARD STATUS : "+enabled);
+        System.out.println("DATE OF EXPIRY: "+expiryMonth);
+        System.out.println("CARD TYPE: "+cardType);
+        System.out.println("LIMIT: "+creditLimit);
+
+        // To check if pin changed 
+        System.out.println("pin: "+pin);
+    }
+    
+    
+    // pin change
+    public void changepin(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter old pin: ");
+        int old_pin=sc.nextInt();
+        if(getpin()==old_pin){
+            System.out.println("Enter new pin: ");
+            int new_pin=sc.nextInt();
+
+            setpin(new_pin);
+
+            
+        }
+    }
+
+    //setter
+    public void setpin(int new_pin){
+                pin = new_pin;
+                System.out.println("The new pin is: "+new_pin);
+            }
+    
+    
+    public void transaction(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount to be withdrawn: ");
+        double amount = sc.nextDouble();
+        
+        if(enabled){
+            if(amount<=creditLimit)
+            {
+                //silver
+                if(cardType==1){
+                double charged_amount=amount-(0.01*amount);
+                
+                System.out.println("Processing........");
+                System.out.println("please wait!!!!");
+                System.out.println("amount "+charged_amount+" processed sucessfully");
+                }
+                //gold
+                else if(cardType==2){
+                double charged_amount=amount-(0.02*amount);
+                
+                System.out.println("Processing........");
+                System.out.println("please wait!!!!");
+                System.out.println("amount "+charged_amount+" processed sucessfully");
+                }
+                //platinum
+                else if(cardType==3){
+                double charged_amount=amount-(0.03*amount);
+                
+                System.out.println("Processing........");
+                System.out.println("please wait!!!!");
+                System.out.println("amount "+charged_amount+" processed sucessfully");
+                }
+            }
+            else{
+                System.out.println("The amount exceed the credit limit of "+creditLimit);
+            }
+        }
+        else
+        {
+            System.out.println("The card has been blocked or disabled!!!\nkindly contact the bank");
+        }
+
+        
+    }
+    
+    public void changestatus(){
+         Scanner sc = new Scanner(System.in);
+        
+             System.out.println("To change the status of your card: ");
+            System.out.println("  Enable - 1\n  Disable/Block - 2");
+            int num = sc.nextInt();
+        if(num==1){
+        setstatus(true);
+        }
+        else if(num==2){
+        setstatus(false);
+        }
+
+
+    }
+    // setter for card status
+    public void setstatus(boolean temp){
+        enabled = temp;
+        System.out.println("The card status is now: "+temp);
+    }
+}
+
+
+public class CreditCardTester{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        Creditcard [] p = new Creditcard[10];
+        
+
+        Creditcard p1 = new Creditcard("Stellin John George", "2022a7ps0003u", true, 1111, "12/12/2026", 1, 1000);
+        Creditcard p2 = new Creditcard("Joe Cijo", "2022a7ps0019u", true, 2222, "12/12/2026", 2, 5000);
+        Creditcard p3 = new Creditcard("BEGAM Yusra Hakim ", "2022a7ps0004u", true, 3333, "12/12/2026", 3, 10000);
+        String []clients={"2022a7ps0003u","2022a7ps0019u","2022a7ps0004u"};
+
+        System.out.println("Enter the card number: ");
+        String CN=sc.nextLine();
+        System.out.println("PIN: ");
+        int PIN = sc.nextInt();
+
+    //check the which client and send index to the function
+        if(CN.equals(clients[0]))
+        {
+        p1.check(PIN);
+            
+        }
+        else if(CN.equals(clients[1]))
+        {
+            p2.check(PIN);
+            
+        }
+        else if(CN.equals(clients[2]))
+        {
+            p3.check(PIN);
+            
+        }
+            else 
+        {
+            System.out.println("The entered card number is wrong ");
+        
+        }
+
+    }
+}
+    
+
---
+++ oops_lab/week04/SalaryDetails.java
@@ -0,0 +1,116 @@
+import java.util.*;
+class Employee{
+private String ID;
+private String name;
+private int department;
+private double basic;
+private double allowance;
+private double totalSalary;
+
+//constructor
+    Employee(String id,String Name,int dept,double base){
+
+        ID=id;
+        name=Name;
+        department=dept;
+        basic=base;
+        
+
+}
+
+public void setTotalSalary(double totalSalary) {
+    this.totalSalary = totalSalary;
+}
+
+public void setAllowance(double allowance) {
+    this.allowance = allowance;
+}
+
+public void display(){
+
+
+    System.out.println("employee id: "+ID);
+    System.out.println("name: "+name);
+    System.out.println("department: "+department);
+    System.out.println(" base salary: "+basic);
+    System.out.println("allowance: "+allowance);
+    System.out.println("total base salary: "+totalSalary);
+
+
+}
+
+
+
+public void salarycalculator(double base,int dept){
+   
+    double HRA=0.4*base;
+
+     if(dept==1){
+     allowance = 1500;
+    }
+     else if(dept==2){
+      allowance = 2500;
+    }
+    else if(dept==3){
+     allowance = 3500;
+    }
+
+    totalSalary=base+allowance+HRA;
+    
+}
+
+
+
+
+}
+
+
+
+
+
+public class SalaryDetails{
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+     
+;
+    
+        System.out.println("Enter the number of employees: ");
+        int n = sc.nextInt();
+        Employee[] e=new Employee[n];
+        for(int i=0;i<n;i++)  {
+            System.out.println("Click 1 for new Employee ");
+            int opt = sc.nextInt();
+            
+            
+            if(opt == 1){
+                sc.nextLine();
+            
+                System.out.println("Enter employee id: ");
+                String id = sc.nextLine();
+                 System.out.println("Enter name: ");
+                String Name = sc.nextLine();
+                System.out.println("Enter employee department:  \n  staff - 1\n  accounts - 2\n  admin - 3");
+                int dept = sc.nextInt();
+                System.out.println("Enter base salary: ");
+                float base = sc.nextFloat();
+            
+                e[i]=new Employee(id,Name,dept, base);
+
+                
+                 
+
+             e[i].salarycalculator(base,dept);
+        
+
+            
+         }  
+         for(int j=0;j<n;j++){
+         e[j].display();
+         }
+
+            
+            
+        
+    }
+}
+}
